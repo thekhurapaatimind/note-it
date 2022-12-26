3324edbda067d0cf03c1d78d84796d3eff5b706d
Abhinav Yadav <actcrick02@gmail.com>
2022-12-26 20:23:28 +0530

    save database

--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ const app = express()
 const port = 3000
 connectToMongo();
 
+app.use(express.json());
+
 //Available Routes
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/notes', require('./routes/notes'));
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/User");
 
-router.get('/', (req,res)=>{
-    res.send('This is auth page');
+//Create a User using POST to "api/auth/"
+router.post('/', (req,res)=>{
+    const user = User(req.body);
+    user.save();
+    res.send(req.body);
 })
 
 module.exports = router
