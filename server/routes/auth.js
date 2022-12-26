01b86df256d9255334a01f95b34ca7f1804e73d2
Abhinav Yadav <actcrick02@gmail.com>
2022-12-26 21:14:37 +0530

    organised auth code

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,23 +3,38 @@ const router = express.Router();
 const User = require("../models/User");
 const { body, validationResult } = require('express-validator');
 
-//Create a User using POST to "api/auth/"
-router.post('/', [
+//Create a New User using POST to "api/auth/createUser"
+router.post('/createUser', [
     body('name'),
     body('email', 'Enter a valid Email').isEmail(),
     body('password', 'Password length should be atleast 5').isLength({min:5})
-] , (req,res)=>{
+] , async (req,res)=>{
+
+    //check for bad request and send the error
     const errors = validationResult(req);
     if(!errors.isEmpty()) {
         return res.status(400).json({errors: errors.array()});
     }
-    User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-    }).then(user => res.json(user))
-    .catch(err=> {console.log(err);
-    res.json({error: 'User email already registered.', message:err.message})});
+
+    //check unique email
+    try {
+        let user = await User.findOne({email: req.body.email});
+        if(user) {
+            return res.status(500).json({error: "User email already registered."})
+        }
+
+        user = await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password
+        })
+
+        res.json(user);
+
+    } catch (err) {
+        console.log(err.message);
+        return res.status(500).send("Some Error Occured")
+    }
 })
 
 module.exports = router
