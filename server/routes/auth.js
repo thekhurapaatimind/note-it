e46df157d4eefc6c36eff2c7d6b914af60ca2e03
Abhinav Yadav <actcrick02@gmail.com>
2022-12-26 20:53:46 +0530

    added validation in auth

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,12 +1,25 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
+const { body, validationResult } = require('express-validator');
 
 //Create a User using POST to "api/auth/"
-router.post('/', (req,res)=>{
-    const user = User(req.body);
-    user.save();
-    res.send(req.body);
+router.post('/', [
+    body('name'),
+    body('email', 'Enter a valid Email').isEmail(),
+    body('password', 'Password length should be atleast 5').isLength({min:5})
+] , (req,res)=>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()});
+    }
+    User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    }).then(user => res.json(user))
+    .catch(err=> {console.log(err);
+    res.json({error: 'User email already registered.', message:err.message})});
 })
 
 module.exports = router
