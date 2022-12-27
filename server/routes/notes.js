8d4aa1243ee5c86754906cb8ef233e0748d9e2e0
Abhinav Yadav <actcrick02@gmail.com>
2022-12-27 23:29:18 +0530

    fetching all notes endpoint

--- server/routes/notes.js
+++ server/routes/notes.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const router = express.Router();
+const Notes = require("../models/Notes");
+const fetchuser = require("../middleware/fetchuser");
+const { body, validationResult } = require('express-validator');
 
-router.get('/', (req,res)=>{
-    res.send('This is routes page');
-})
+
+
+//Route 1: Fetch all notes using GET request
+router.get('/mynotes', fetchuser, async (req,res)=>{
+    
+    try {
+        const notes = await Notes.find({user: req.user.id})
+        res.json(notes);
+
+    } catch (error) {
+        console.log(err.message);
+        return res.status(500).send("Some Error Occured")
+    }
+})  
 
 module.exports = router
