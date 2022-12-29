e1c499076f5f0b4495e303d7936bdd03d6f19c49
Abhinav Yadav <actcrick02@gmail.com>
2022-12-29 20:34:36 +0530

    add and route components

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,34 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/Home'
+import NavbarComponent from './components/NavbarComponent'
+import ErrorPage from './components/ErrorPage'
 
 function App() {
+
+  const [mode, setmode] = useState('light');
+  const [bgColor, setbgColor] = useState('#E6E9EA')
+  document.body.style.backgroundColor = bgColor;
+  
+  const toggleMode = () => {
+    if(mode==='dark') {
+      setmode('light');
+      setbgColor('#E6E9EA');
+    }
+    else {
+      setmode('dark');
+      setbgColor('#000124');
+    }
+  }
   return (
-    <div className="App">
-      <h1>Note-It</h1>
-    </div>
+    <>
+      <NavbarComponent mode={mode} toggleMode={toggleMode}/>
+      <Routes>
+        <Route exact path='/' element={<Home/>}/>
+        <Route exact path='/about' element={<About/>}/>
+        <Route exact path='/*' element={<ErrorPage/>}/>
+      </Routes>
+    </>
   );
 }
 
