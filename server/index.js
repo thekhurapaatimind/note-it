fc7caa98475e88a240a5e88dc8b634030571ca87
Abhinav Yadav <actcrick02@gmail.com>
2023-01-10 19:12:27 +0530

    installed cors

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 const connectToMongo = require("./db")
 const express = require('express')
+var cors = require('cors')
 
 const app = express()
 const port = 5000
 connectToMongo();
 
 app.use(express.json());
+app.use(cors());
 
 //Available Routes
 app.use('/api/auth', require('./routes/auth'));
